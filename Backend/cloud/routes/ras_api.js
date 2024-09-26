4046688a49eb0951d863d052de8ed911c3d34731
DreaserousProductions <ec2-user@ip-172-31-14-36.ec2.internal>
2024-09-26 05:23:37 +0000

    Creating temp route for raspberry image upload

---
+++ Backend/cloud/routes/ras_api.js
@@ -0,0 +1,9 @@
+// cloudBackend/routes/api.js
+
+const express = require('express');
+const router = express.Router();
+const { uploadImage } = require('../controllers/imageController');
+
+router.post('/upload', uploadImage);
+
+module.exports = router;
